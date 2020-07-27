82de4b4886d388a7a74537b38b5f6770757e2c07
Viadsten <stanulevich.vg@yandex.ru>
2020-07-27 17:35:45 +0300

    test mobile adaptive

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ var before__button = document.querySelector(".example-cat__button--before");
 var after__button = document.querySelector(".example-cat__button--after");
 var before__image = document.querySelector(".example-cat__img--before");
 var after__image = document.querySelector(".example-cat__img--after");
-var nav__button = document.querySelector(".main-nav__toggle");
-var button = document.getElementById('nav-btn')
-var button__active = document.querySelector(".main-nav__toggle--active");
 
 after__button.onclick = function() {
   after__image.style.display = "block";
@@ -17,12 +14,4 @@ before__button.onclick = function() {
   after__image.style.display = "none";
 };
 
-button.onclick = function() {
-  if (button.className != "button__active"){
-    button.classList.add("button__active");
-  }
-  else {
-    button.classList.remove("button__active");
-  }
-};
 
