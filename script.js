0225692bb2524b316cbcf6bb403203316af28f6f
Viadsten <stanulevich.vg@yandex.ru>
2020-08-04 18:45:42 +0300

    everything wrong exept mobile fix version

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var before__button = document.querySelector(".example-cat__button--before");
 var after__button = document.querySelector(".example-cat__button--after");
 var before__image = document.querySelector(".example-cat__img--before");
 var after__image = document.querySelector(".example-cat__img--after");
+var navToggle = document.querySelector(".main-nav__toggle");
+var mainNav = document.querySelector(".main-nav");
 
 after__button.onclick = function() {
   after__image.style.display = "block";
@@ -15,3 +17,14 @@ before__button.onclick = function() {
 };
 
 
+navToggle.addEventListener('click', function(){
+  if (navToggle.classList.contains('main-nav__toggle--after'))
+  {
+    navToggle.classList.remove('main-nav__toggle--after');
+    mainNav.classList.add('visually-hidden')
+  }
+  else{
+    navToggle.classList.add('main-nav__toggle--after');
+    mainNav.classList.remove('visually-hidden')
+  }
+});
